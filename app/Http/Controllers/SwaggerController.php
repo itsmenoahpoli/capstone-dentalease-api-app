a51b85ca9e92523ca7b82502dce3559fc15e4397
Patrick Policarpio <patrickpolicarpio08@gmail.com>
2025-08-04 13:14:34 +0800

    fix

--- app/Http/Controllers/SwaggerController.php
+++ app/Http/Controllers/SwaggerController.php
@@ -15,10 +15,22 @@ class SwaggerController extends Controller
 
     public function docs(): Response
     {
-        $yamlContent = file_get_contents(base_path('api-docs.yaml'));
+        // Try to read YAML file first, fallback to JSON
+        $yamlPath = storage_path('api-docs/api-docs.yaml');
+        $jsonPath = storage_path('api-docs/api-docs.json');
 
-        return response($yamlContent, 200, [
-            'Content-Type' => 'application/x-yaml',
+        if (file_exists($yamlPath)) {
+            $content = file_get_contents($yamlPath);
+            $contentType = 'application/x-yaml';
+        } elseif (file_exists($jsonPath)) {
+            $content = file_get_contents($jsonPath);
+            $contentType = 'application/json';
+        } else {
+            abort(404, 'API documentation not found. Please run: php artisan l5-swagger:generate');
+        }
+
+        return response($content, 200, [
+            'Content-Type' => $contentType,
         ]);
     }
 }
